cc8c1b7b7cc48d0bc3f674c5184dcc45660f84c3
xF3Zx <48673228+xF3Zx@users.noreply.github.com>
2019-03-20 06:02:52 +0700

    Add files via upload

---
+++ F3ZKIDZ.sh
@@ -0,0 +1,394 @@
+#!/bin/bash
+#mau ngerecode izin dulu gan
+#ni mau izin di sini
+#085738995966 Wa
+#FB F-RREL ID
+
+clear
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+clear
+
+echo -e " 		$green ╲╲╲╲╲┏━━━━┓╱╱╱╱╱"
+echo -e " 		$green ╭┓┏╮┏┫╰╯╰╯┣┓╭┓┏╮"
+echo -e " 		$green ┃┗┛┃┗┫┗━━┛┣┛┃┗┛┃"
+echo -e " 		$green ╰┳┳╯┏┻━━━━┻┓╰┳┳╯"
+echo -e " 		$green ╱┣┗┳┫┈▕╲▂╱▏┣┳┛┫╲"
+echo -e " 		$green ╱╰┻┻┫┈▕▕♡▏▏┣┻┻╯╲"
+echo -e "		$green ╱╱╱╱┃┈▕╱▔╲▏┃╲╲╲╲"
+echo -e "$blue|===|#######################################|===|"
+echo -e "$blue|===|$green Author by : F3Z || F-RREL             $blue|===|"
+echo -e "$blue|===|$green Format Code : Bash                    $blue|===|"
+echo -e "$blue|===|$green tanggal rillis : 25 Maret 2019        $blue|===|"
+echo -e "$blue|===|$green Facebook : F-RREL ID                  $blue|===|"
+echo -e "$blue|===|$green WA : 085738995966                     $blue|===|"
+echo -e "$blue|===|$green Tools Installer                       $blue|===|"
+echo -e "$blue|===|$green Version : 1.0                         $blue|===|"
+echo -e "$blue|===|#######################################|===|"
+read -p '   [ Masukan Nama Anda ]   : ' nama1
+sleep 1
+echo -e $cyan ' Selamat Datang Dan Menggunakan Tools Ini : ' $nama1
+sleep 1
+clear
+
+echo -e "               $green ╲╲╲╲╲┏━━━━┓╱╱╱╱╱"
+echo -e "               $green ╭┓┏╮┏┫╰╯╰╯┣┓╭┓┏╮"
+echo -e "               $green ┃┗┛┃┗┫┗━━┛┣┛┃┗┛┃"
+echo -e "               $green ╰┳┳╯┏┻━━━━┻┓╰┳┳╯"
+echo -e "               $green ╱┣┗┳┫┈▕╲▂╱▏┣┳┛┫╲"
+echo -e "               $green ╱╰┻┻┫┈▕▕♡▏▏┣┻┻╯╲"
+echo -e "               $green ╱╱╱╱┃┈▕╱▔╲▏┃╲╲╲╲"
+echo -e "$blue|===|#######################################|===|"
+echo -e "$blue|===|$green Author by : F3Z || F-RREL             $blue|===|"
+echo -e "$blue|===|$green Format Code : Bash                    $blue|===|"
+echo -e "$blue|===|$green tanggal rillis : 25 Maret 2019        $blue|===|"
+echo -e "$blue|===|$green Facebook : F-RREL ID                  $blue|===|"
+echo -e "$blue|===|$green WA : 085738995966                     $blue|===|"
+echo -e "$blue|===|$green Tools Installer                       $blue|===|"
+echo -e "$blue|===|$green Version : 1.0                         $blue|===|"
+echo -e "$blue|===|#######################################|===|"
+echo -n ' [ Masukkan Password Untuk Menjalankan Tools ]  : '
+read pw; 
+
+if [ $pw = 'F3Z' ];
+then
+    echo -e $g 'Password anda benar'
+else
+    echo -e $red 'upss anda noobz'
+fi
+
+sleep 1
+clear
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[P**********]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Pl*********]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Ple********]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Plea*******]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Pleas******]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Please*****]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Please ****]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Please W***]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Please Wa**]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Please Wai*]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+
+echo -e "$g▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇>$p[Please Wait]$g<▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇▇"
+
+sleep 0.1
+
+clear
+
+echo -e "$cyan"
+figlet -f slant "BOJONEGORO CYBER SECURITY"
+echo -e " 		$green ╲╲╲╲╲┏━━━━┓╱╱╱╱╱"
+echo -e " 		$green ╭┓┏╮┏┫╰╯╰╯┣┓╭┓┏╮"
+echo -e " 		$green ┃┗┛┃┗┫┗━━┛┣┛┃┗┛┃"
+echo -e " 		$green ╰┳┳╯┏┻━━━━┻┓╰┳┳╯"
+echo -e " 		$green ╱┣┗┳┫┈▕╲▂╱▏┣┳┛┫╲"
+echo -e " 		$green ╱╰┻┻┫┈▕▕♡▏▏┣┻┻╯╲"
+echo -e "		$green ╱╱╱╱┃┈▕╱▔╲▏┃╲╲╲╲"
+echo -e "$blue|===|#######################################|===|"
+echo -e "$blue|===|$green Author by : F3Z || F-RREL             $blue|===|"
+echo -e "$blue|===|$green Format Code : Bash                    $blue|===|"
+echo -e "$blue|===|$green tanggal rillis : 25 Maret 2019        $blue|===|"
+echo -e "$blue|===|$green Facebook : F-RREL ID                  $blue|===|"
+echo -e "$blue|===|$green WA : 085738995966                     $blue|===|"
+echo -e "$blue|===|$green Tools Installer                       $blue|===|"
+echo -e "$blue|===|$green Version : 1.0                         $blue|===|"
+echo -e "$blue|===|#######################################|===|"
+echo""
+sleep 1
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo -e "$cyan|###|$green             ALL NAME TOOLS           $cyan |###|$green On /$red Off $cyan|###|"
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo -e "$cyan|###|$green 1.|>Tools PlayBoy ( Installer )      $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 2.|>Tools BAJINGANv6                 $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 3.|>Tools DrakFly                    $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 4.|>Tools Tuan Badut                 $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo""
+sleep 1
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo -e "$cyan|###|$green              TOOLS NAME              $cyan |###|$green On /$red Off $cyan|###|"
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo -e "$cyan|###|$green 5.|>Spam Jd.Id                       $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 6.|>Tools Spammer                    $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 7.|>Santet-Online                    $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 8.|>GcoSpam                          $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo""
+sleep 1
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo -e "$cyan|###|$green              TOOLS NAME              $cyan |###|$green On /$red Off $cyan|###|"
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo -e "$cyan|###|$green 9.|>Google-Dork                      $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 10.|>Insta Hack                      $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 11.|>Ddos Hammer                     $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|$green 12.|>script deface creator           $cyan |###|$green    ON $cyan   |###|"
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+echo -e "$cyan|###|$green 00.|>EXIT                            $cyan |###|$red   Exit  $cyan |###|"
+echo -e "$cyan|###|+++++++++++++++++++++++++++++++++++++++|###|++++++++++|###|"
+
+echo""
+echo -e $blue
+read -p "╭─[Pilih Angkane Cok ]
+╰─F3Z@ROOT=•>" p
+
+if [ $p = 1 ] || [ $p = 1 ]
+then 
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+pkg install git
+git clone https://github.com/Pity27/PlayBoy
+echo -e "$green T E R I N S A L L"
+fi
+
+if [ $p = 2 ] || [ $p = 2 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+pip2 install termcolor
+pip2 install lolcat
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 3 ] || [ $p = 3 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+apt install python2
+pip2 install scapy
+pip2 install requests
+pip2 install mechanize
+apt install hydra
+git clone https://github.com/Ranginang67/DarkFly-Tool
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 4 ] || [ $p = 4 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 5 ] || [ $p = 5 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+apt install php
+apt install curl
+curl https://pastebin.com/raw/9BYy1JVc -o jdid.php
+echo -e "$green T E R I N S T A L L"
+fi
+
+if  [ $p = 6 ] || [ $p = 6 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "SWORD X CYB3R"
+echo -e "$yellow MENGINSTALL"
+pkg update && pkg upgrade -y
+pkg install python2
+pkg install php
+pkg install toilet
+pkg install nano ruby figlet
+gem install lolcat
+pkg install git
+git clone  https://github.com/BLACheat78/spammblackheat78
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 7 ] || [ $p = 7 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+apt install python2
+git clone https://github.com/Gameye98/santet-online
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 8 ] || [ $p = 8 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+apt install git
+git clone https://github.com/Amriez/gcospam
+echo -e "$green  T E R I N S T A L L"
+fi
+
+if [ $p = 9 ] || [ $p = 9 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+pkg install git
+pkg install python2
+git clone https://github.com/XG77Z10/Google-Dork
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 10 ] || [ $p = 10 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+pkg install python
+pkg install git
+git clone https://github.com/avramit/instahack
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 11 ] || [ $p = 11 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+pkg install python
+git clone http://github.com/cyweb/hammer
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 12 ] || [ $p = 12 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "F3Z"
+echo -e "$yellow MENGINSTALL"
+apt update && apt upgrade -y
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+echo -e "$green T E R I N S T A L L"
+fi
+
+if [ $p = 00 ] || [ $p = 00 ]
+then
+clear
+echo -e "$cyan"
+figlet -f big "BOJONEGORO CYBER SECURITY"
+sleep 1
+echo -e "               $green ╲╲╲╲╲┏━━━━┓╱╱╱╱╱"
+echo -e "               $green ╭┓┏╮┏┫╰╯╰╯┣┓╭┓┏╮"
+echo -e "               $green ┃┗┛┃┗┫┗━━┛┣┛┃┗┛┃"
+echo -e "               $green ╰┳┳╯┏┻━━━━┻┓╰┳┳╯"
+echo -e "               $green ╱┣┗┳┫┈▕╲▂╱▏┣┳┛┫╲"
+echo -e "               $green ╱╰┻┻┫┈▕▕♡▏▏┣┻┻╯╲"
+echo -e "               $green ╱╱╱╱┃┈▕╱▔╲▏┃╲╲╲╲"
+echo -e "$blue|===|#######################################|===|"
+echo -e "$blue|===|$green Author by : F3Z || F-RREL             $blue|===|"
+echo -e "$blue|===|$green Format Code : Bash                    $blue|===|"
+echo -e "$blue|===|$green tanggal rillis : 25 Maret 2019        $blue|===|"
+echo -e "$blue|===|$green Facebook : F-RREL ID                  $blue|===|"
+echo -e "$blue|===|$green WA : 085738995966                     $blue|===|"
+echo -e "$blue|===|$green Tools Installer                       $blue|===|"
+echo -e "$blue|===|$green Version : 1.0                         $blue|===|"
+echo -e "$blue|===|#######################################|===|"
+sleep 1
+echo -e "$cyan Thanks You for using my tools"
+sleep 1
+echo -e "$blue Saran Anda Bisa saya ketahui setelah kalian mengirim saran diWa Saya"
+sleep 1
+echo -e "$yellow 085738995966"
+sleep 1
+echo -e "$cyan GOODBEY script Kiddies script Yg ngak bermanfaat :)"
+sleep 1
+exit
+fi
